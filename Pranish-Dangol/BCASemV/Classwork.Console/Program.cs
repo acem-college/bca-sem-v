d1e4ee8a8b3f93fc2d9f58521e395e26f00065d5
pranish <pranishdangol123@gmail.com>
2023-12-07 07:14:42 +0545

    console,mvc,api

---
+++ Pranish-Dangol/BCASemV/Classwork.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstConsoleApp
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First console app");
+            Console.ReadKey();
+        }
+    }
+}

fdada9c1709fb59945f96fe84172659232336d29
mhzndenish <maharjandenish65@gmail.com>
2023-12-11 21:07:28 +0545

    classwork

---
+++ Denish-Maharjan/Ui/Ui/Models/Form.cs
@@ -0,0 +1,9 @@
+﻿namespace Ui.Models
+{
+    public class Form
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}

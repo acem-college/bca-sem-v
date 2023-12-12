9d201e9828e9ccb305907e9ef828cf6a6a8a7f3a
saroj10 <vattasaroj@gmail.com>
2023-12-12 22:02:31 +0545

    interface

--- Saroj-Bhatta/BCASemV/Classwork.Console/Program.cs
+++ Saroj-Bhatta/BCASemV/Classwork.Console/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Classwork.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Use interface
+IUserInput userInput = new UserInput();
+var xResult = userInput.AskForValue("x");
+var yResult = userInput.AskForValue("y");
+
+// Use interface
+ICalculator calculator = new Calculator();
+var addResult = calculator.Add(xResult, yResult);
+var subResult = calculator.Subtract(xResult, yResult);
+
+Console.WriteLine($"Add result: {addResult}");
+Console.WriteLine($"Sub result: {subResult}");
+

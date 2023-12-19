802284bd24ac40eb8991adda9689d9f958fffbff
lokraj55 <ojhalokraj2@gmail.com>
2023-12-19 08:07:26 +0545

    lok mvc commit

---
+++ lok/BCASemV/MyMvc.Ui/Application/Models/Users/ListUserVM.cs
@@ -0,0 +1,8 @@
+﻿namespace MyMvc.Ui.Application.Models.Users
+{
+    public class ListUserVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ lok/BCASemV/MyMvc.Ui/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMvc.Ui.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

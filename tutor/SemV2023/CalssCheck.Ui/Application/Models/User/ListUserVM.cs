69fcf6829d8b0279cfce9c957b0e29c8e8ec33b8
Santosh Shrestha <sshrestha@ap-logic.com>
2023-12-19 08:05:22 +0545

    tutor main commit

---
+++ tutor/SemV2023/CalssCheck.Ui/Application/Models/User/ListUserVM.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassCheck.Ui.Application.Models.User
+{
+    public class ListUserVM
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}

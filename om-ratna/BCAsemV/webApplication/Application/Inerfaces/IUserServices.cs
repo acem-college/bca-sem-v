ae0d251a9c8ab0dfcd5df267a979d1a40b4d031a
im-Om123 <ombajracharya4@gmail.com>
2023-12-24 22:05:13 +0545

    Om-

--- om-ratna/BCAsemV/webApplication/Application/Inerfaces/IUserServices.cs
+++ om-ratna/BCAsemV/webApplication/Application/Inerfaces/IUserServices.cs
@@ -5,6 +5,7 @@ namespace webApplication.Application.Inerfaces
 {
     public interface IUserServices
     {
+        Task<SaveUserVM> GetAsync(Guid userId, CancellationToken cancellationToken);
         Task<List<ListUserVM>> ListAsync(CancellationToken cancellationToken);
     }
 }
--- om-ratna/BCAsemV/webApplication/Application/Services/UserService.cs
+++ om-ratna/BCAsemV/webApplication/Application/Services/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using webApplication.Application.Inerfaces;
 using webApplication.Application.Model.User;
+using webApplication.ui.Infrastructure.Persistence;
 
 namespace webApplication.Application.Services
 {
@@ -7,16 +8,41 @@ namespace webApplication.Application.Services
     {
         public async Task<List<ListUserVM>> ListAsync(CancellationToken cancellationToken)
         {
-            var users = new List<ListUserVM>();
-            users.Add(new ListUserVM
+            var users = Users.Profiles.Select(s => new ListUserVM
             {
-                Id = Guid.NewGuid(),
-                FirstName = "om",
-                LastName = "Bajracharya",
-                Email = "ombajrachrya4@gmail.com",
-                PhoneNumber = "99999999999"
-            });
+                Email = s.Email,
+                FirstName = s.FirstName,
+                LastName = s.LastName,  
+                Id = s.Id,
+                PhoneNumber = s.PhoneNumber,
+            }).ToList();
             return users;
+        }
+         
+    public async Task CreateAsync(SaveUserVM user, CancellationToken cancellationToken)
+        {
+            Users.Profiles.Add(new Domain.Entities.UserProfile
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            });
+        }
+
+        public async Task<SaveUserVM> GetAsync(Guid userId, CancellationToken cancellationToken)
+        {
+            var user = Users.Profiles.Where(w => w.Id == userId)
+               .Select(s => new SaveUserVM
+               {
+                   Id = s.Id,
+                   FirstName = s.FirstName,
+                   LastName = s.LastName,
+                   Email = s.Email,
+                   PhoneNumber = s.PhoneNumber
+               }).FirstOrDefault();
+            return user;
         }
     }
 }

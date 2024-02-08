3035374fddc5ba95c115507c8c48f3a8eb02531a
Rubek <rubekmhzn7@gmail.com>
2024-02-08 07:28:11 +0545

    deleted previous files

--- rubek-maharjan/ClassWork.MvcUi/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ClassWork.MvcUi.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}

36e467417f2f0771bff4291bb2f5b874dec34f9f
basnetbijay <bijay.basnet2057@gmail.com>
2024-02-08 07:29:07 +0545

    new push

--- Bijay12/BCASemV/Classwork.MVC/Application/Interfaces/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Classwork.MVC.Application.Model.User;
-
-namespace Classwork.MVC.Application.Interfaces
-{
-    public interface IUserService
-    {
-        Task<List<ListUserVM>>ListAsync(CancellationToken cancellationToken);
-    }
-}

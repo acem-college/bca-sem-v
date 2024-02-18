05feedeb24f9ea80d7d287bcae9d12d75e636d39
Bishal Lama <bshallama16@gmail.com>
2024-02-18 09:06:21 +0545

    Bishal LAma:Project Setup

---
+++ Bishal-Lama/bca-sem-v/Wcs/WaterConsumptionSystem.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WaterConsumptionSystem.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Bishal-Lama/bca-sem-v/Wcs/WaterConsumptionSystem.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WaterConsumptionSystem.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
